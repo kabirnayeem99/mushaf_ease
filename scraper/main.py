d1027c4cc3b23cc2f295edc56883790eea8756b0
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-06-18 04:06:24 +0600

    chore: remove unnecessary comments

--- scraper/main.py
+++ scraper/main.py
@@ -3,16 +3,12 @@ from src.files import read_json_files
 from src.db import create_database, insert_surah_data
 
 def main():
-    # Directory where JSON files are stored
     json_dir = './json_files/'
 
-    # Read JSON files
     json_data_list = read_json_files(json_dir)
 
-    # Create SQLite database
     db_conn = create_database()
 
-    # Insert data into database
     for surah_data in json_data_list:
         insert_surah_data(db_conn, surah_data)
 
